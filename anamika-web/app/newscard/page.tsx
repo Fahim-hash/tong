4d839159d3c6ad49e80eb4b4a44c1f407fbf43eb
Fahim-hash <82995127+Fahim-hash@users.noreply.github.com>
2026-05-16 20:37:11 +0600

    Update page.tsx

--- anamika-web/app/newscard/page.tsx
+++ anamika-web/app/newscard/page.tsx
@@ -1,1 +1,11 @@
-newscard
+import AuthGuard from '@/components/AuthGuard';
+
+export default function NewsCardPage() {
+  return (
+    <AuthGuard>
+      <div className="p-8">
+        <h1>Welcome to the Protected News Card Generator!</h1>
+      </div>
+    </AuthGuard>
+  );
+}
